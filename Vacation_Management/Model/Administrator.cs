28d6e73f1367c1638eec6d80b481f65b2520d3d7
prokkaly <prokkaly@stud.uni-obuda.hu>
2023-10-05 18:01:46 +0200

    User groups created

---
+++ Vacation_Management/Model/Administrator.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vacation_Management.Model
+{
+    public class Administrator
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Vacation_Management/Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vacation_Management.Model
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
