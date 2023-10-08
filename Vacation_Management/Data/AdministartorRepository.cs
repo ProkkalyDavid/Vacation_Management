1ad3ef33a04f6fcd07521766704723ffc2ac949d
prokkaly <prokkaly@stud.uni-obuda.hu>
2023-10-08 19:00:05 +0200

    AdministratorRepo

---
+++ Vacation_Management/Data/AdministartorRepository.cs
@@ -0,0 +1,37 @@
+﻿using Vacation_Management.Model;
+
+namespace Vacation_Management.Data
+{
+    public class AdministartorRepository
+    {
+        List<Administrator> administrators;
+
+        public AdministartorRepository()
+        {
+            administrators = new List<Administrator>();
+        }
+
+        public void Create(Administrator administrator)
+        {
+            administrators.Add(administrator);
+        }
+        public IEnumerable<Administrator> Read()
+        {
+            return administrators;
+        }
+        public Administrator Read(string name)
+        {
+            return administrators.FirstOrDefault(t => t.Name == name);
+        }
+        public void Update(Administrator administrator)
+        {
+            var old = Read(administrator.Name);
+            //promote, depromote, name, email
+        }
+        public void Delete(Administrator administrator)
+        {
+            var administratorToDelete = Read(administrator.Name);
+            administrators.Remove(administratorToDelete);
+        }
+    }
+}
