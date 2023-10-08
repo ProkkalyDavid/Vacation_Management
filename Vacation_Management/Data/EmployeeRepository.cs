f00dd21d2867aa9640e1686c47f31e54f40554d0
prokkaly <prokkaly@stud.uni-obuda.hu>
2023-10-08 18:23:12 +0200

    EmpolyeeRepo

---
+++ Vacation_Management/Data/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using Vacation_Management.Model;
+
+namespace Vacation_Management.Data
+{
+    public class EmployeeRepository
+    {
+        List<Employee> employees;
+
+        public EmployeeRepository()
+        {
+            employees = new List<Employee>();
+        }
+
+        public void Create(Employee employee)
+        {
+            employees.Add(employee);
+        }
+        public IEnumerable<Employee> Read()
+        {
+            return employees;
+        }
+        public Employee Read(string name)
+        {
+            return employees.FirstOrDefault(t => t.Name == name);
+        }
+        public void Update(Employee employee)
+        {
+            var old = Read(employee.Name);
+            //promote, depromote, name, email
+        }
+        public void Delete(Employee employee)
+        {
+            var employeeToDelete = Read(employee.Name);
+            employees.Remove(employeeToDelete);
+        }
+    }
+}
