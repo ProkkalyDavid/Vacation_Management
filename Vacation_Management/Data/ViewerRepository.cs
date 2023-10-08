3850dcaa3ef43c90d0488ebd6ac1981719304474
prokkaly <prokkaly@stud.uni-obuda.hu>
2023-10-08 17:43:38 +0200

    ViewerRepo

---
+++ Vacation_Management/Data/ViewerRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using Vacation_Management.Model;
+
+namespace Vacation_Management.Data
+{
+    public class ViewerRepository
+    {
+        List<Viewer> viewers;
+        public ViewerRepository()
+        {
+            viewers = new List<Viewer>();
+        }
+
+        public void Create(Viewer viewer)
+        {
+            viewers.Add(viewer);
+        }
+        public IEnumerable<Viewer> Read()
+        {
+            return viewers;
+        }
+        public Viewer Read(string name)
+        {
+            return viewers.FirstOrDefault(t => t.Name == name);
+        }
+        public void Update(Viewer viewer)
+        {
+            var old = Read(viewer.Name);
+            //promote, new email, name, etc
+        }
+        public void Delete(string name)
+        {
+            var viewer = Read(name);
+            viewers.Remove(viewer);
+        }
+    }
+}
