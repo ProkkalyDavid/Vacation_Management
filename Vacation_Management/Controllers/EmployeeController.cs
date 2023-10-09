e0c1f8e936c53276d73f61b6f8478a565b6c3c9b
prokkaly <prokkaly@stud.uni-obuda.hu>
2023-10-09 17:33:36 +0200

    EmplyeeController

---
+++ Vacation_Management/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vacation_Management.Data;
+
+namespace Vacation_Management.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IEmployeeRepository repository;
+
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CalendarView()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateLeaveRequest()
+        {
+            return View();
+        }
+    }
+}
