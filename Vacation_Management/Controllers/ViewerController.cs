362c00b88113b5aae3857f57bcff260066d121a3
prokkaly <prokkaly@stud.uni-obuda.hu>
2023-10-09 17:07:09 +0200

    ViewerController

---
+++ Vacation_Management/Controllers/ViewerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using Vacation_Management.Data;
+
+namespace Vacation_Management.Controllers
+{
+    public class ViewerController : Controller
+    {
+        IViewerRepository repository;
+        public ViewerController(IViewerRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View(this.repository.Read());
+        }
+
+        [HttpGet]
+        public IActionResult CalendarView()
+        {
+            return View();
+        }
+    }
+}
